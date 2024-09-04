51ed6801d4d8ec5a6d6ea7e5f7a83eb315b88125
Joseph Acheampong <acheampongjoseph470@gmail.com>
2024-09-04 09:24:15 +0000

    tracking added

--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const http = require('http')
+const {Server} = require('socket.io')
 const connectDB = require('./config/db');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
@@ -10,14 +12,30 @@ const mechanicRoutes = require('./routes/mechanicRoutes')
 dotenv.config()
 connectDB()
 
-const app = express()
 app.use(express.json())
 app.use(morgan('dev'))
 app.use(cors())
 
+const app = express()
+const server = http.createServer(app)
+const io = new Server(server)
+
 app.use('/api/auth', authRoutes)
 app.use('/api/cars', carRoutes)
 app.use('/api/mechanics', mechanicRoutes)
+
+io.on('connection', (socket) => {
+    console.log('New client connected');
+    
+    socket.on('updateMechanicLocation', (locationData) => {
+        io.emit('mechanicLocationUpdated', locationData)
+    })
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+        
+    })
+})
 
 app.listen(5000, () => console.log('Server running on port 5000')
 )
