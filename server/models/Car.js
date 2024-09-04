832e018255ef45be2995c73e93a2c3debeb1c734
Joseph Acheampong <acheampongjoseph470@gmail.com>
2024-09-04 07:46:03 +0000

    car schema added

---
+++ server/models/Car.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+
+const CarSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    make: {
+        type: String,
+        required: true
+    },
+    model: {
+        type: String,
+        required: true
+    },
+    issueDescription: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['reported', 'in-progress', 'resolved'],
+        default: 'reported'
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    }
+})
+
+module.exports = mongoose.model('Car', CarSchema)
