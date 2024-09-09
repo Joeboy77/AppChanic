0d6e80a144fedd7c852df59a18bb82a3c7fb6b23
Joseph Acheampong <acheampongjoseph470@gmail.com>
2024-09-09 14:34:02 +0000

    update

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,43 +1,20 @@
 const User = require('../models/User')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
 
-exports.registerUser = async(req, res) => {
-    const {name, email, password} = req.body
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: '/api/auth/google/callback'
+}, async(accessToken, refreshToken, profile, done) => {
     try{
-        let user = await User.findOne({email})
-        if(user){
-            return res.status(400).json({msg: 'User already exist'})
-        }
-        user = new User({name, email, password: await bcrypt.hash(password, 10)})
-        await user.save()
+        let user = await User.findOne({googleId: profile.id})
+        if(user) return done(null, user)
 
-        const token = jwt.sign({id: user.id}, process.env.JWT_SECRET, {expiresIn: '1h'})
-        res.json({token})
-    } catch(err) {
-        console.error(err);
-        res.status(500).send('Server Error')
+        user = new User({
+            
+        })    
     }
-}
-
-exports.loginUser = async(req, res) => {
-    const {email, password} = req.body
-
-    try{
-        const user = await User.findOne({email})
-
-        if(!user){
-            return res.status(400).json({ msg: 'Invalid credentials'})
-        }
-        const isMatch = await bcrypt.compare(password, user.password)
-        if(!isMatch) {
-            return res.status(400).json({ msg: 'Invalid credentials'})
-        }
-
-        const token = jwt.sign({id: user.id}, process.env.JWT_SECRET, {expiresIn: '1h'})
-        res.json({token})
-    } catch(err){
-        console.error(err);
-        res.status(500).send('Server Error')
-    }
-}
+}))
